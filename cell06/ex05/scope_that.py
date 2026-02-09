94d3a0c9a1a439a0bfeb6598a906be9a3806974e
KiwwyIsDev <tanakornreal@gmail.com>
2026-02-09 18:02:44 +0700

    feat: add ex05

---
+++ cell06/ex05/scope_that.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def add_one(x):
+    x = x + 1
+
+a = 10
+print(a)
+add_one(a)
+print(a)
+
+# I see a value after use add_one doesn't change cause It's have scope of function
