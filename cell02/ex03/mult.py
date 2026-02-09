bc9b9c2ca2fde4bb5160c5a310740dd9590464d3
KiwwyIsDev <tanakornreal@gmail.com>
2026-02-09 13:13:38 +0700

    feat: add ex03

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,12 @@
+print("Enter the first number:")
+fnum = int(input())
+print("Enter the second number:")
+snum = int(input())
+result = fnum * snum
+print(f"{fnum} x {snum} = {result}")
+if result == 0:
+    print("This number is both positive and negative.")
+elif result > 0:
+    print("This number is positive.")
+else:
+    print("This number is negative.")
